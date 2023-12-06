ef15088645b3468405843565ec4ac5f1c2eb49ca
fsollfrank <88027250+fsollfrank@users.noreply.github.com>
2023-12-06 13:10:56 +0100

    Added Interface Name Translation

--- stp.py
+++ stp.py
@@ -1,25 +1,15 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-# (c) 2018 Heinlein Support GmbH
-#          Robert Sander <r.sander@heinlein-support.de>
-
-# This is free software;  you can redistribute it and/or modify it
-# under the  terms of the  GNU General Public License  as published by
-# the Free Software Foundation in version 2.  check_mk is  distributed
-# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
-# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
-# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
-# tails. You should have  received  a copy of the  GNU  General Public
-# License along with GNU Make; see the file  COPYING.  If  not,  write
-# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
-# Boston, MA 02110-1301 USA.
-
 from .agent_based_api.v1 import *
 
 def parse_stp(string_table):
     section = []
-    for line in string_table:
+    for i in range(len(string_table[1])-2):
+        for x in range(len(string_table[2])):
+            if string_table[1][i][1] == string_table[2][x][0]:
+                string_table[0][i][0] = string_table[2][x][1]
+    for line in string_table[0]:
         for item in line:
             section.append(item)
     return section
@@ -38,6 +28,19 @@ register.snmp_section(
                 "4", # STP Port Enable
                 "5", # STP Designated Root
             ]),
+        SNMPTree(
+            base=".1.3.6.1.2.1.17.1.4.1"
+            oid=[
+                "1", # Bridge Port number
+                "2", # Interface Index
+            ]),
+        SNMPTree(
+            base=".1.3.6.1.2.1.2.2.1"
+            oid=[
+                "1", # Interface Index
+                "2", # Interface Name
+            ]
+        )
     ],
 )
 
